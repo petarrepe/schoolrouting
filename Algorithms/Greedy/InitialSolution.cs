babb64da8fcd2ac282a48d1b6b9b1874582b70ba
Petar Stojanović <petar.stojanovic@fer.hr>
2018-01-14 18:18:03 +0100

    added InitialSolution to tracker

---
+++ Algorithms/Greedy/InitialSolution.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Greedy
+{
+    public static class InitialSolution
+    {
+        public static Solution Find(List<Cluster> clusterList, double maxCapacity)
+        {
+            var sortedByNumberOfStudents = clusterList.OrderBy(t => t.Count()).ToList();
+            List<List<int>> busTours = new List<List<int>>();
+
+            for (int i = 0; i < sortedByNumberOfStudents.Count;)
+            {
+                List<int> tour = new List<int>();
+                int capacitySum = 0;
+                while (sortedByNumberOfStudents[i].Count() + capacitySum <= maxCapacity)
+                {
+                    capacitySum += sortedByNumberOfStudents[i].Count();
+                    tour.Add(sortedByNumberOfStudents[i].StopIndex);
+                    ++i;
+                    if (i == sortedByNumberOfStudents.Count()) break;
+                }
+                busTours.Add(tour);
+            }
+            return new Solution(busTours, clusterList);
+        }
+    }
+}
